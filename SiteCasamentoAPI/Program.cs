53523eb8f700f7bf14f2f955362d05c6d789d708
Lucas Paiva <lucas.costa@solutis.com.br>
2025-02-20 17:09:01 -0300

    liberando swagger

--- SiteCasamentoAPI/Program.cs
+++ SiteCasamentoAPI/Program.cs
@@ -20,15 +20,15 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
         c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
     });
-}
+// }
 
 var devClient = "http://localhost:4200";
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithOrigins(devClient));
